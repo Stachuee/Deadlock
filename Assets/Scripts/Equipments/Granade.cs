53c3ff9121b487173f18c75fc5fa3bddcf299543
Vladyslav Dovhalskyy <vlad32cj@gmail.com>
2023-03-13 23:05:47 +0100

    Granade logic added

--- Assets/Scripts/Equipments/Granade.cs
+++ Assets/Scripts/Equipments/Granade.cs
@@ -9,6 +9,9 @@ public class Granade : MonoBehaviour
     [SerializeField] float fuseDuration;
     float explosionTimer;
 
+    [SerializeField] float damage = 20f;
+    [SerializeField] float explosionRadius;
+
     private void Start()
     {
         explosionTimer = fuseDuration + Time.time;    
@@ -25,6 +28,22 @@ public class Granade : MonoBehaviour
     private void Explode()
     {
         Debug.Log("Boom");
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                ITakeDamage target = collider.GetComponent<ITakeDamage>();
+                if (target != null) target.TakeDamage(damage, DamageType.Bullet);
+            }
+        }
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a wire sphere to show the explosion radius in the editor
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
